b5a596664e525e9e94ea320b16619f0633fcc0e2
Ashuttosh pattanaik <114045671+aashu1012@users.noreply.github.com>
2025-06-21 15:37:14 +0530

    Initial commit

--- config/firebase.js
+++ config/firebase.js
@@ -8,7 +8,19 @@ const __dirname = dirname(__filename);
 const require = createRequire(import.meta.url);
 
 // Initialize Firebase Admin SDK
-const serviceAccount = require('./cloudtasker-bdfcb-firebase-adminsdk-fbsvc-a41846fec6.json');
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
